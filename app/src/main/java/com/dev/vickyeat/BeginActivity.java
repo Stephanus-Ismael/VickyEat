21f5d325c5987d8e905084a8465e2e04af4d032d
Stephan <stephan.1101101@gmail.com>
2022-10-01 05:53:37 +0200

    Initial commit

---
+++ app/src/main/java/com/dev/vickyeat/BeginActivity.java
@@ -0,0 +1,36 @@
+package com.dev.vickyeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class BeginActivity extends AppCompatActivity {
+    private ConstraintLayout regBtn;
+    private ConstraintLayout logBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_begin);
+
+        regBtn=findViewById(R.id.regBtn);
+        logBtn=findViewById(R.id.logBtn);
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(BeginActivity.this, RegisterActivity.class));
+            }
+        });
+
+        logBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(BeginActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
