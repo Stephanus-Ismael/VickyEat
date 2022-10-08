54cbb6eacd65e7d2e75f411d7265c5de64ad3fb7
Stephan <stephan.1101101@gmail.com>
2022-10-08 16:01:07 +0200

    HomeActivity in progress v2...

--- app/src/main/java/com/dev/vickyeat/HomeActivity.java
+++ app/src/main/java/com/dev/vickyeat/HomeActivity.java
@@ -1,14 +1,41 @@
 package com.dev.vickyeat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.dev.vickyeat.Adaptor.CategoryAdaptor;
+import com.dev.vickyeat.Domain.CategoryDomain;
+
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewCategoryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        recyclerViewCategory();
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewCategoryList=findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> category=new ArrayList<>();
+        category.add(new CategoryDomain("Breakfast", "cat_1"));
+        category.add(new CategoryDomain("Lunch", "cat_2"));
+        category.add(new CategoryDomain("Fast Food", "cat_3"));
+        category.add(new CategoryDomain("Drinks", "cat_4"));
+
+        adapter = new CategoryAdaptor(category);
+        recyclerViewCategoryList.setAdapter(adapter);
     }
 }
